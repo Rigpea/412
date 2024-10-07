b0559b2a487839656c3c2db50562ab8b67aff7a6
Rigpea <rigpea@bu.edu>
2024-10-07 16:34:51 -0400

    Export

--- cs412/settings.py
+++ cs412/settings.py
@@ -86,14 +86,18 @@ WSGI_APPLICATION = "cs412.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 
-DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
-}
-
-# Optional: Ensure SSL is required when connecting to PostgreSQL in production
-if 'DATABASE_URL' in os.environ:
-    DATABASES['default']['OPTIONS'] = {'sslmode': 'require'}
-
+if os.environ.get('DATABASE_URL'):
+    DATABASES = {
+        'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    }
+else:
+    # Fallback to SQLite for local development
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
