c3a76fbff10f745d29e03db5c5c623b5eac24b82
Rigpea <rigpea@bu.edu>
2024-10-07 15:26:01 -0400

    Created Profile model for mini_fb

--- mini_fb/models.py
+++ mini_fb/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    email = models.EmailField()
+    profile_image_url = models.URLField(max_length=500)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
