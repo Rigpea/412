3f8f1133fe08a6bdb2eb96696df0a7d836d53814
Rigpea <rigpea@bu.edu>
2024-10-14 14:15:37 -0400

    Task 4 complete

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import ShowAllProfilesView, ShowProfilesPageView, CreateProfileView
+from .views import ShowAllProfilesView, ShowProfilesPageView, CreateProfileView, CreateStatusMessageView
 
 urlpatterns = [
     path('', ShowAllProfilesView.as_view(), name='show_all_profiles'),
     path('profile/<int:pk>/', ShowProfilesPageView.as_view(), name='show_profile'),
     path('create_profile/', CreateProfileView.as_view(), name='create_profile'),
+    path('profile/<int:pk>/create_status',CreateStatusMessageView.as_view(), name='create_status')
+
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -2,9 +2,9 @@
 
 
 from django.shortcuts import render
-from .models import Profile
+from .models import Profile, StatusMessage
 from django.views.generic import ListView, DetailView, CreateView
-from .forms import CreateProfileForm
+from .forms import CreateProfileForm, CreateStatusMessageForm
 from django.urls import reverse
 
 # Create your views here.
@@ -37,3 +37,23 @@ class CreateProfileView(CreateView):
 
     def get_success_url(self):
         return reverse('show_profile', kwargs={'pk': self.object.pk})
+    
+class CreateStatusMessageView(CreateView):
+    model = StatusMessage
+    form_class = CreateStatusMessageForm
+    template_name = 'mini_fb/create_status_form.html'
+
+    def get_context_data(self, **kwargs):
+        profile = Profile.objects.get(pk=self.kwargs['pk'])
+        context = {
+            'profile': profile,
+            'form': self.get_form(self.form_class)
+        }
+        return context
+    def form_valid(self, form):
+        profile = Profile.objects.get(pk=self.kwargs['pk'])
+        form.instance.profile = profile
+        return super().form_valid(form)
+
+    def get_success_url(self):
+        return reverse('show_profile', kwargs={'pk': self.kwargs['pk']}) 
