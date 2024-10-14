369006be07159be82e7e66e989a3acbe55c8491f
Rigpea <rigpea@bu.edu>
2024-10-14 11:41:18 -0400

    Task 1 complete

--- mini_fb/urls.py
+++ mini_fb/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import ShowAllProfilesView
+from .views import ShowAllProfilesView, ShowProfilesPageView
 
 urlpatterns = [
     path('', ShowAllProfilesView.as_view(), name='show_all_profiles'),
+    path('profile/<int:pk>/', ShowProfilesPageView.as_view(), name='show_profile'),
+
 ]
--- mini_fb/views.py
+++ mini_fb/views.py
@@ -1,6 +1,9 @@
+#mini_fb views.py 
+
+
 from django.shortcuts import render
 from .models import Profile
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
 
@@ -8,3 +11,16 @@ class ShowAllProfilesView(ListView):
     model = Profile
     template_name = 'mini_fb/show_all_profiles.html'
     context_object_name = 'profiles'
+
+# Thiw view needs to obtain data for one profile record, and 
+#to deleguate work to a templaete called 
+#show_profile.htlm to display that profile needs to render
+class ShowProfilesPageView(DetailView):
+    model = Profile
+    template_name = 'mini_fb/show_profile.html'
+    context_object_name = 'profiles'  # or 'profile'
+    
+
+    # gotta now pass it the profile how? 
+    # need to perform a get operatino from model 
+
